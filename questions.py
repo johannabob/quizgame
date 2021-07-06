7155107712e9e0fd8abb02bde1664c1e5cb45129
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-06 10:06:55 +0300

    Initial questions

---
+++ questions.py
@@ -0,0 +1,5 @@
+questions_list = [
+    ["When do cherry trees blossom?", "1: Winter", "2: Spring", "3: Summer", "4: Autumn", 2]
+    ["When do we start our lunch break?", "1: 10.30", "2: 11.00", "3: 11.30", "4: 12.00", 3]
+]
+
