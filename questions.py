7d86bdba72cb88156b6ae0ad4b050631d7e90741
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-06 10:16:28 +0300

    Renamed file and added questions

--- questions.py
+++ questions.py
@@ -1,5 +1,6 @@
 questions_list = [
     ["When do cherry trees blossom?", "1: Winter", "2: Spring", "3: Summer", "4: Autumn", 2]
     ["When do we start our lunch break?", "1: 10.30", "2: 11.00", "3: 11.30", "4: 12.00", 3]
+    ["Will you pass the certification exam?", "1: Yes", "2: No", "3: Maybe", "4: On the second try", 1]
 ]
 
